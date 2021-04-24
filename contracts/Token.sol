affe6e974428d3b6d57c04fb08767a9ab469c6d3
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-04-24 10:02:25 +1000

    Add packages

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,20 +4,11 @@ pragma solidity >=0.4.22 <0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is Ownable {
-  address public owner = msg.sender;
-  uint public last_completed_migration;
 
-  modifier restricted() {
-    require(
-      msg.sender == owner,
-      "This function is restricted to the contract's owner"
-    );
-    _;
-  }
-
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+  constructor() ERC20("Block52", "B52") {
+    _mint(52000, msg.sender);
   }
 }
