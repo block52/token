87d79e1f3b1c774a72a35ac5b003d8a910961224
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-04-25 12:59:39 +1000

    Chore: Fix contracts

--- truffle-config.js
+++ truffle-config.js
@@ -6,6 +6,26 @@ module.exports = {
     "truffle-plugin-verify"
   ],
   networks: {
+    kovan: {
+      provider: () => {
+        return new HDWalletProvider({
+          mnemonic: {
+            phrase: process.env.MNEMONIC
+          },
+          providerOrUrl: process.env.NODE
+        })
+      },
+      network_id: "42",
+      gas: 4500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+    development: {
+      host: "localhost", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*" // Any network (default: none)
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -16,7 +36,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.4",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.7.0",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
